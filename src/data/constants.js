9a731faaff7b3e385d627e89694955d8a33fcf4b
Ujjwal-Patel-02 <157982245+Ujjwal-Patel-02@users.noreply.github.com>
2024-08-04 16:22:16 +0530

    projects changes

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Ujjwal-Patel-02",
   resume:
-    "https://drive.google.com/file/d/1mCIyDvDezOzrFstWMudaix1lTrEGpy6-/view?usp=drive_link",
+    "https://drive.google.com/file/d/1mCIyDvDezOzrFstWMudaix1lTrEGpy6-/view?usp=sharing",
   linkedin: "http://www.linkedin.com/in/ujjwal-patel-727854287",
   insta: "https://www.instagram.com/urs.truely.ujjwal/",
 };
